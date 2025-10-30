4f01b90b68a2f747fd22e902ef1dd24e82be9c88
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-30 17:52:39 -0500

    commit - HUD .

--- src/main/java/com/example/juegoaviones/hud/HUD.java
+++ src/main/java/com/example/juegoaviones/hud/HUD.java
@@ -1,4 +1,22 @@
 package com.example.juegoaviones.hud;
 
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.scene.Node;
+
 public class HUD {
+    private Node guiNode;
+    private BitmapFont guiFont;
+    private BitmapFont hudText;
+    private BitmapText messageText;
+
+    public HUD(Node guiNode, BitmapFont guiFont) {
+        this.guiNode = guiNode;
+        this.guiFont = guiFont;
+        init();
+    }
+
+    private void init() {
+        
+    }
 }
