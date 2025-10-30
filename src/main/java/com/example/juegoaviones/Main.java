396e57528f5d611f6dbe9efb52470e837ef98679
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-30 18:23:57 -0500

    commit - Main :.

--- src/main/java/com/example/juegoaviones/Main.java
+++ src/main/java/com/example/juegoaviones/Main.java
@@ -3,6 +3,6 @@ package com.example.juegoaviones;
 public class Main {
     public static void main(String[] args) {
         GameApp app = new GameApp();
-     //   app.start();
+        app.start();
     }
 }
